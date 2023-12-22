79587309a52340dabe75e360791505eca6ca3a86
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2023-12-21 22:24:52 -0500

    varios sonidos

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
-let sonido;
+let sonido = [];
 
 function preload() {
-  soundFormats("wav", "mp3", "ogg");
-  sonidoMeMeMe = loadSound("sonido/MeMeMe.mp3");
+  for (let i = 0; i < 7; i++) {
+    sonido[i] = loadSound("sonido/" + i + ".mp3");
+  }
 }
 
 function setup() {
@@ -11,13 +12,95 @@ function setup() {
 }
 
 function presionado() {
-  sonidoMeMeMe.play();
+  let r = Math.floor(random(7));
   circle(mouseX, mouseY, 20);
+
+  switch (r) {
+    case 0:
+      sonido[0].play();
+      break;
+    case 1:
+      sonido[1].play();
+      break;
+    case 2:
+      sonido[2].play();
+      break;
+    case 3:
+      sonido[3].play();
+      break;
+    case 4:
+      sonido[4].play();
+      break;
+    case 5:
+      sonido[5].play();
+      break;
+    case 6:
+      sonido[6].play();
+      break;
+  }
 }
 
 function draw() {}
-
 function mouseDragged() {
-  sonidoMeMeMe.play();
+  let r = Math.floor(random(7));
   circle(mouseX, mouseY, 20);
+
+  switch (r) {
+    case 0:
+      sonido[0].play();
+      break;
+    case 1:
+      sonido[1].play();
+      break;
+    case 2:
+      sonido[2].play();
+      break;
+    case 3:
+      sonido[3].play();
+      break;
+    case 4:
+      sonido[4].play();
+      break;
+    case 5:
+      sonido[5].play();
+      break;
+    case 6:
+      sonido[6].play();
+      break;
+  }
 }
+
+function keyPressed() {
+  switch (key) {
+    case "a":
+      sonido[0].play();
+      break;
+    case "s":
+      sonido[1].play();
+      break;
+    case "d":
+      sonido[2].play();
+      break;
+    case "f":
+      sonido[3].play();
+      break;
+    case "g":
+      sonido[4].play();
+      break;
+    case "h":
+      sonido[5].play();
+      break;
+    case "j":
+      sonido[6].play();
+      break;
+  }
+}
+
+//if (variable + valor )
+
+//switch varias preguntas de la misma variable
+//switch (animal){
+// case "gato"
+//print(" si es gato")
+// break
+//}
